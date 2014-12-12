a1ebbc630cf3ce7ef053ac0cf84522ea31632e91
gggmyyy <gmyoung321@gmail.com>
2014-12-12 15:00:30 +1300

    learning signal r, doesn't work though

---
+++ c#/ggak-final-project/ggak-final-project/Controllers/LocationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Ajax.Utilities;
+
+namespace ggak_final_project.Controllers
+{
+    public class LocationController : ApiController
+    {
+        // GET: api/Location
+        public DateTime Get()
+        {
+           DateTime currentTime =  DateTime.UtcNow;
+            return currentTime;
+        }
+
+        // GET: api/Location/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Location
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Location/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Location/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
