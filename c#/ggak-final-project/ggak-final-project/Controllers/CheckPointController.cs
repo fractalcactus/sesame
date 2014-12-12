860c280e032e1db013f184cf7330a23408ac23c5
gggmyyy <gmyoung321@gmail.com>
2014-12-12 16:41:11 +1300

    documentation added

--- c#/ggak-final-project/ggak-final-project/Controllers/CheckPointController.cs
+++ c#/ggak-final-project/ggak-final-project/Controllers/CheckPointController.cs
@@ -10,28 +10,50 @@ namespace ggak_final_project.Controllers
     public class CheckPointController : ApiController
     {
         // GET: api/CheckPoint
+        /// <summary>
+        /// This is the summary of this method
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET: api/CheckPoint/5
+        /// <summary>
+        /// Creates checkpoint with a specific Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string Get(int id)
         {
             return "value";
         }
 
         // POST: api/CheckPoint
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
         public void Post([FromBody]string value)
         {
         }
 
         // PUT: api/CheckPoint/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE: api/CheckPoint/5
+        /// <summary>
+        /// Deletes checkpoint from the map
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
         }
