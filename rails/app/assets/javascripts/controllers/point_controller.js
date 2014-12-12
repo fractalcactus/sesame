1b14840b363e698408e9472ba5bad8070bde38b5
Katherine Anderson <apprentice@Samoa.local>
2014-12-12 22:52:53 +1300

    Working location

--- rails/app/assets/javascripts/controllers/point_controller.js
+++ rails/app/assets/javascripts/controllers/point_controller.js
@@ -1,14 +1,68 @@
-var Controller = function(){
-  this.self = this;
-}
+$(document).ready( function() {
 
-Controller.prototype = {
-  getLocation: function() {
-      navigator.geolocation.getCurrentPosition(showPosition);
-  },
-  showPosition: function(position) {
-      console.log("argh")
-      x.innerHTML = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
+  var map;
+  function initialize() {
+    var mapOptions = {
+      center: { lat: -41.295291, lng: 174.773071},
+      zoom: 18
+    };
+
+    // Grabbing the map div to send the new position to
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    // Getting your current location if your browser allows it
+
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+    // Pops up info box with comment
+
+        var infowindow = new google.maps.InfoWindow({
+          map: map,
+          position: pos,
+          content: "www.youtube.com"
+        });
+
+    // Centers map on current position
+        checkLocation(pos);
+        map.setCenter(pos);
+
+    // Response if there is an error getting the geolocation
+
+      }, function() { handleNoGeolocation(true);});
+    }
+    else {
+
+    // Browser doesn't support Geolocation
+
+      handleNoGeolocation(false);
+    }
   }
-}
+
+  // Error flags if there is a problem
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+      var content = 'Error: The Geolocation service failed.';
+    } else {
+      var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+      map: map,
+      position: new google.maps.LatLng(60, 105),
+      content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+})
+
+
