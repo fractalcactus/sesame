f66b2d3e9a19d26b205b9eedd3b2f7e06ac90911
gggmyyy <gmyoung321@gmail.com>
2014-12-15 12:09:59 +1300

    Radius controller working. Tested and Passed

--- c#/ggak-final-project/ggak-final-project.Tests/UnitTest1.cs
+++ c#/ggak-final-project/ggak-final-project.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ggak_final_project.Models;
+using ggak_final_project.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
@@ -8,6 +9,30 @@ namespace ggak_final_project.Tests
     [TestClass]
     public class UnitTest1
     {
-        
+        [TestMethod]
+        public void RadiusReturnsTrue()
+        {
+            //arrange
+            RadiusController radius = new RadiusController();
+            //act
+            bool actual = radius.pointRadius(-41.288402, 174.776165, -41.288402, 174.776165);
+            //assert
+            Assert.AreEqual(true,actual);
+
+        }
+
+        [TestMethod]
+        public void RadiusReturnsFalse()
+        {
+            //arrange
+            RadiusController radius = new RadiusController();
+            bool actual = radius.pointRadius(-41.288402, 174.776165, -41.289188, 174.775961);
+
+            //Assert
+            Assert.AreEqual(false,actual);
+        }
+
+
+
     }
 }
