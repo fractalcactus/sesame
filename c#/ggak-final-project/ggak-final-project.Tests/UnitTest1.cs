d18a17ef761a05ed306b5c1d575636dd6852d89f
gggmyyy <gmyoung321@gmail.com>
2014-12-15 17:59:58 +1300

    Radius tested and working

--- c#/ggak-final-project/ggak-final-project.Tests/UnitTest1.cs
+++ c#/ggak-final-project/ggak-final-project.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ggak_final_project.BusinessLogic;
 using ggak_final_project.Models;
 using ggak_final_project.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,27 +10,163 @@ namespace ggak_final_project.Tests
     [TestClass]
     public class UnitTest1
     {
+
+        private double radusThreshold = 0.001;
+
         [TestMethod]
-        public void RadiusReturnsTrue()
+        public void Radius_TwoPointsAreClose_WhenOnTopOfEachOrher()
         {
             //arrange
-            RadiusController radius = new RadiusController();
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat;
+            double secondLong = firstLong;
+
             //act
-            bool actual = radius.pointRadius(-41.288402, 174.776165, -41.288402, 174.776165);
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong,secondLat,secondLong);
             //assert
-            Assert.AreEqual(true,actual);
+            Assert.IsTrue(actual);
 
         }
 
         [TestMethod]
-        public void RadiusReturnsFalse()
+        public void Radius_TwoPointsAreClose_WhenLatudeIsSame_AND_LongisOffByExactlyOne_radusThreshold()
         {
             //arrange
-            RadiusController radius = new RadiusController();
-            bool actual = radius.pointRadius(-41.288402, 174.776165, -41.289188, 174.775961);
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat ;
+            double secondLong = firstLong - radusThreshold;
 
-            //Assert
-            Assert.AreEqual(false,actual);
+            //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong,secondLat,secondLong);
+            //assert
+            Assert.IsTrue(actual);
+
+        }
+
+        [TestMethod]
+        public void Radius_TwoPointsAreNOTClose_WhenLatudeIsSame_AND_LongisOffBy_TWO_radusThreshold()
+        {
+            //arrange
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat;
+            double secondLong = firstLong - (radusThreshold * 2);
+
+            //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong, secondLat, secondLong);
+            //assert
+            Assert.IsFalse(actual);
+
+        }
+
+        [TestMethod]
+        public void Radius_TwoPointsAreNOTClose_WhenLAT_isOffBy_ONE_AND_LONGisOffBy_ONE_radusThreshold()
+        {
+            //arrange
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat - (radusThreshold * 1);
+            double secondLong = firstLong - (radusThreshold * 1);
+
+                //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong, secondLat, secondLong);
+            //assert
+            Assert.IsFalse(actual);
+
+        }
+
+        [TestMethod]
+        public void Radius_TwoPointsAreNOTClose_WhenLatude_isOffBy_sin45_AND_LongisOffBy_sin45_radusThreshold()
+        {
+            //arrange
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat - (radusThreshold * Math.Sin(45));
+            double secondLong = firstLong - (radusThreshold * Math.Sin(45));
+
+            //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong, secondLat, secondLong);
+            //assert
+            Assert.IsTrue(actual);
+
+        }
+
+
+
+
+        [TestMethod]
+        public void Radius_TwoPointsAreClose_WhenLatudeIsSame_AND_LongisGreaterByExactlyOne_radusThreshold()
+        {
+            //arrange
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat;
+            double secondLong = firstLong + radusThreshold;
+
+            //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong, secondLat, secondLong);
+            //assert
+            Assert.IsTrue(actual);
+
+        }
+
+        [TestMethod]
+        public void Radius_TwoPointsAreNOTClose_WhenLatudeIsSame_AND_LongisGreaterBy_TWO_radusThreshold()
+        {
+            //arrange
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat;
+            double secondLong = firstLong + (radusThreshold * 2);
+
+            //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong, secondLat, secondLong);
+            //assert
+            Assert.IsFalse(actual);
+
+        }
+
+        [TestMethod]
+        public void Radius_TwoPointsAreNOTClose_WhenLAT_isGreaterBy_ONE_AND_LONGisGreaterBy_ONE_radusThreshold()
+        {
+            //arrange
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat + (radusThreshold * 1);
+            double secondLong = firstLong + (radusThreshold * 1);
+
+            //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong, secondLat, secondLong);
+            //assert
+            Assert.IsFalse(actual);
+
+        }
+
+        [TestMethod]
+        public void Radius_TwoPointsAreNOTClose_WhenLatude_isGreaterBy_sin45_AND_LongisGreaterBy_sin45_radusThreshold()
+        {
+            //arrange
+            Radius radius = new Radius();
+            double firstLat = -41.288402;
+            double firstLong = 174.776165;
+            double secondLat = firstLat + (radusThreshold * Math.Sin(45));
+            double secondLong = firstLong + (radusThreshold * Math.Sin(45));
+
+            //act
+            bool actual = radius.AreTwoPointsClose(firstLat, firstLong, secondLat, secondLong);
+            //assert
+            Assert.IsTrue(actual);
+
         }
 
 
---
+++ c#/ggak-final-project/ggak-final-project/BusinessLogic/Radius.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ggak_final_project.BusinessLogic
+{
+    public class Radius
+    {
+        private double radiusThreshold = 0.001; //fraction of a degree
+        private double accuracy { get { return radiusThreshold / 100.0; } } //1% of the threshold
+
+        private double Pythag(double pointOne, double pointTwo)
+        {
+            double sides = (Math.Pow(pointOne, 2)) + (Math.Pow(pointTwo, 2));
+            double answer = Math.Sqrt(sides);
+            return answer;
+        }
+
+        public Boolean AreTwoPointsClose(double wayPointLat, double wayPointLong, double userLat, double userLong)
+        {
+
+            double largerLat;
+            double smallerLat;
+
+            double largerLong;
+            double smallerlong;
+
+            if (wayPointLat > userLat)
+            {
+                largerLat = wayPointLat;
+                smallerLat = userLat;
+            }
+            else
+            {
+                largerLat = userLat;
+                smallerLat = wayPointLat;
+            }
+            if (wayPointLong > userLong)
+            {
+                largerLong = wayPointLong;
+                smallerlong = userLong;
+            }
+            else
+            {
+                largerLong = userLong;
+                smallerlong = wayPointLong;
+            }
+
+
+            double latDelta = (largerLat - smallerLat);
+            double longDelta = (largerLong - smallerlong);
+            return Pythag(latDelta, longDelta) < (radiusThreshold + accuracy);
+
+            //    double latLessThreshold = latDelta - radiusThreshold;
+            //    double longLessThreshold = longDelta - radiusThreshold;
+
+
+            //    return (latLessThreshold < accuracy) && (longLessThreshold < accuracy);
+
+
+
+            //if (accuracy > (Pythag(wayPointLat + userLat) - radiusThreshold) && (accuracy > (userLat - wayPointLat - radiusThreshold)))
+            //{
+            //    if (accuracy > (wayPointLong - userLong - radiusThreshold) && (accuracy > (userLong - wayPointLong - radiusThreshold)))
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+        }
+
+
+    }
+}
