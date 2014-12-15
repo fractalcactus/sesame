514693231ca0c285d21878b8d5444e3b83e1a621
gggmyyy <gmyoung321@gmail.com>
2014-12-15 15:28:43 +1300

    darcy database now with less migration

--- c#/ggak-final-project/ggak-final-project/Migrations/201412130403326_updated-to-float.cs
+++ c#/ggak-final-project/ggak-final-project/Migrations/201412130403326_updated-to-float.cs
@@ -7,8 +7,8 @@ namespace ggak_final_project.Migrations
     {
         public override void Up()
         {
-            AlterColumn("dbo.WayPoints", "Lat", c => c.Single(nullable: false));
-            AlterColumn("dbo.WayPoints", "Lng", c => c.Single(nullable: false));
+            AlterColumn("dbo.WayPoints", "Lat", c => c.Single(nullable: true));
+            AlterColumn("dbo.WayPoints", "Lng", c => c.Single(nullable: true));
         }
         
         public override void Down()
