775aa82efad817bc36b81b22929d944de6fe4369
gggmyyy <gmyoung321@gmail.com>
2014-12-16 16:07:44 +1300

    testing waypoint controler

--- c#/ggak-final-project/ggak-final-project.Tests/UnitTest1.cs
+++ c#/ggak-final-project/ggak-final-project.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.Entity;
 using System.Linq;
@@ -18,27 +19,33 @@ namespace ggak_final_project.Tests
         private IWorldPlaygroundDBContext _context;
         private WayPointsController _controller;
 
-        [ClassInitialize]
+       [TestInitialize]
         public void SetUp()
         {
-           _context  = new fakeDataContext();
+             _context  = new fakeDataContext();
+           
             WayPoint w1 = new WayPoint();
             w1.Id = 1;
             w1.Latitude = (float) 41.234343;
             w1.Longitude = (float) 43.234343;
             w1.URL = "W1URL";
-            _context.WayPoints.Add(w1);
+          
             WayPoint w2 = new WayPoint();
-            w1.Id = 2;
-            w1.Latitude = (float) 44.234343;
-            w1.Longitude = (float) 45.234343;
-            w1.URL = "W2URL";
-            WayPoint w3 = new WayPoint();
-            w1.Id = 3;
-            w1.Latitude = (float) 49.234343;
-            w1.Longitude = (float) 48.234343;
-            w1.URL = "W3URL";
+            w2.Id = 2;
+            w2.Latitude = (float) 44.234343;
+            w2.Longitude = (float) 45.234343;
+            w2.URL = "W2URL";
 
+            WayPoint w3 = new WayPoint();
+            w3.Id = 3;
+            w3.Latitude = (float) 49.234343;
+            w3.Longitude = (float) 48.234343;
+            w3.URL = "W3URL";
+
+
+            _context.WayPoints.Add(w1);
+            _context.WayPoints.Add(w2);
+            _context.WayPoints.Add(w3);
             _controller = new WayPointsController(_context);
 
         }
@@ -48,8 +55,11 @@ namespace ggak_final_project.Tests
         [TestMethod]
         public void is_in_radius_with_exact_coords()
         {
+            //arrange
+           // SetUp();
             //act
         WayPoint checkWayPoint =  _controller.GetWayPoint("41.234343", "43.234343");
+            //assert
             Assert.AreEqual("W1URL",checkWayPoint.URL);
 
         }
@@ -57,18 +67,13 @@ namespace ggak_final_project.Tests
 
     public class fakeDataContext : IWorldPlaygroundDBContext
     {
+        public fakeDataContext()
+        {
+            this.WayPoints = new FakeDbSet<WayPoint>();
+        }
 
-        public System.Data.Entity.IDbSet<WayPoint> WayPoints { get;  set; }
-        //{
-        //    get
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //    set
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
+        public IDbSet<WayPoint> WayPoints { get;  set; }
+      
 
         public int SaveChanges()
         {
@@ -83,7 +88,7 @@ namespace ggak_final_project.Tests
     }
 
 
-    public class FakeDbSet<T> : IDbSet<T> where T :WayPoint
+    public class FakeDbSet<T> : IDbSet<T> where T : class
     {
         private ObservableCollection<T> _data;
         private IQueryable _query;
@@ -94,6 +99,11 @@ namespace ggak_final_project.Tests
             _query = _data.AsQueryable();
         }
 
+        public FakeDbSet(List<T> fakeCollection )
+        {
+            _data = new ObservableCollection<T>(fakeCollection);
+            _query = _data.AsQueryable();
+        }
         public T Add(T item)
         {
             _data.Add(item);
@@ -123,7 +133,7 @@ namespace ggak_final_project.Tests
 
         public System.Collections.ObjectModel.ObservableCollection<T> Local
         {
-            get { throw new NotImplementedException(); }
+            get { return _data; }
         }
 
         public T Remove(T item)
@@ -132,87 +142,88 @@ namespace ggak_final_project.Tests
             return item;
         }
 
-        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        //public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
             return _data.GetEnumerator();
         }
 
-        public Type ElementType
-        {
-            get { return _query.ElementType; }
-        }
+        //public Type ElementType
+        //{
+        //    get { return _query.ElementType; }
+        //}
 
-        public System.Linq.Expressions.Expression Expression
-        {
-            get { throw new NotImplementedException(); }
-        }
+        //public System.Linq.Expressions.Expression Expression
+        //{
+        //    get { throw new NotImplementedException(); }
+        //}
 
-        public System.Linq.IQueryProvider Provider
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-
+        //public System.Linq.IQueryProvider Provider
+        //{
+        //    get { throw new NotImplementedException(); }
+        //}
 
 
-        T IDbSet<T>.Add(T entity)
-        {
-            throw new NotImplementedException();
-        }
 
-        T IDbSet<T>.Attach(T entity)
-        {
-            throw new NotImplementedException();
-        }
+
+        //T IDbSet<T>.Add(T item)
+        //{
+        //    _data.Add(item);
+        //    return item;
+        //}
+
+        //T IDbSet<T>.Attach(T entity)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         TDerivedEntity IDbSet<T>.Create<TDerivedEntity>()
         {
-            throw new NotImplementedException();
+            return Activator.CreateInstance<TDerivedEntity>();
         }
 
-        T IDbSet<T>.Create()
-        {
-            throw new NotImplementedException();
-        }
+        //T IDbSet<T>.Create()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        T IDbSet<T>.Find(params object[] keyValues)
-        {
-            throw new NotImplementedException();
-        }
+        //T IDbSet<T>.Find(params object[] keyValues)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        ObservableCollection<T> IDbSet<T>.Local
-        {
-            get { throw new NotImplementedException(); }
-        }
+        //ObservableCollection<T> IDbSet<T>.Local
+        //{
+        //    get { throw new NotImplementedException(); }
+        //}
 
-        T IDbSet<T>.Remove(T entity)
-        {
-            throw new NotImplementedException();
-        }
+        //T IDbSet<T>.Remove(T entity)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         System.Collections.Generic.IEnumerator<T> System.Collections.Generic.IEnumerable<T>.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return _data.GetEnumerator();
         }
 
         Type IQueryable.ElementType
         {
-            get { throw new NotImplementedException(); }
+            get { return _query.ElementType; }
         }
 
         System.Linq.Expressions.Expression IQueryable.Expression
         {
-            get { throw new NotImplementedException(); }
+            get { return _query.Expression; }
         }
 
         IQueryProvider IQueryable.Provider
         {
-            get { throw new NotImplementedException(); }
+            get { return _query.Provider; }
         }
     }
 
